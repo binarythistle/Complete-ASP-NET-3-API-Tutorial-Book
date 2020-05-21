3669f4057b7a68b7241e8eb244397719dd0909f2
Les Jackson <les.jackson@binarythistle.com>
2020-05-21 14:32:08 +1000

    Added Command Model to API Project

---
+++ src/CommandAPI/Models/Commands.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CommandAPI.Models
+{
+    public class CommandAPI
+    {
+        [Key]
+        [Required]
+        public int Id {get; set;}
+        
+        [Required]
+        [MaxLength(250)]
+        public string HowTo {get; set;}
+        
+        [Required]
+        public string Platform {get; set;}
+        
+        [Required]
+        public string CommandLine {get; set;}
+    }
+}
