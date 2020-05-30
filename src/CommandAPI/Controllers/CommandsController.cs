f2af09ac68e88f8ad9ad82387b4d7974077f35db
Les Jackson <les.jackson@binarythistle.com>
2020-05-30 16:41:45 +1000

    Added a reminder to clean up code

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -13,6 +13,8 @@ namespace CommandAPI.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
+        //Random change
+
         private readonly ICommandAPIRepo _repository;
         private readonly IMapper _mapper;
 
